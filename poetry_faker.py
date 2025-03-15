57103909d14a57a7bd02c64a4c613ad2cf0a00d4
Juanse Marquez <juansemarquez@yahoo.com.ar>
2025-03-15 15:11:40 -0300

    Initial commit

---
+++ poetry_faker.py
@@ -0,0 +1,31 @@
+class PoetryFaker:
+
+    def __init__(self, text, breakline = "\n", prepend = "_", language = "es"):
+        self.breakline = breakline
+        self.prepend = prepend
+        self.language = language
+        self.text = text
+
+    def verse_maker(self):
+        print(self.eliminate_punctuation())
+
+    def eliminate_punctuation(self):
+        letters_and_space=" abcdefghijklmnopqrstuvwxyz"
+        if self.language == "es":
+            letters_and_space += "áéíóúüñ¡¿"
+        total = len(self.text)
+        i = 0
+        output = ""
+        while i < total:
+            if self.text[i] == self.prepend:
+                output += self.text[i+1] + "\n"
+                i+=2
+                continue
+            if self.text[i] in letters_and_space or self.text[i] in letters_and_space.upper():
+                output += self.text[i]
+            else:
+                if self.text[i] in ("?", "!"):
+                    output+=self.text[i]
+                output += "\n"
+            i+=1
+        return output.replace("\n ", "\n")
